63ba187950d53ce9f284732de1893fa4ba54e954
benofben <ben.lackey@outlook.com>
2017-09-27 17:08:10 -0700

    closer...

--- report.py
+++ report.py
@@ -3,7 +3,10 @@ import csv
 
 def run():
     filenames=get_filenames()
-    print(filenames)
+    for filename in filenames:
+        #[hourly, byol, legacy] =
+        x=process_file(filename)
+        print(str(x))
 
 def get_filenames():
     filenames=[]
@@ -11,5 +14,24 @@ def get_filenames():
         if file.endswith(".csv"):
             filenames.append(os.path.join("./reports", file))
     return filenames
+
+def process_file(filename):
+    hourly=0
+    byol=0
+    legacy=0
+
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if 'VM Image' in row['OfferType']:
+                date = ['UsageStartDate']
+                if 'silver_support' in row['ServicePlanName'] or 'hourly_pricing' in row['ServicePlanName']:
+                    hourly+=float(row['Usage'])
+                elif 'byol' in row['ServicePlanName']:
+                    byol+=float(row['Usage'])
+                else:
+                    legacy+=float(row['Usage'])
+
+    return [hourly, byol, legacy]
 
 run()
