ed99c38f946b65258d7228c1f1da6e3f63801655
benofben <ben.lackey@outlook.com>
2017-09-27 18:45:21 -0700

    skeleton

--- payout.py
+++ payout.py
@@ -1,4 +1,16 @@
+import csv
+from dateutil.parser import parse
+
 def run():
+    process_file()
+
+def process_file():
+    with open('payout.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            process_row(row)
+
+def process_row(row):
     pass
 
 run()
